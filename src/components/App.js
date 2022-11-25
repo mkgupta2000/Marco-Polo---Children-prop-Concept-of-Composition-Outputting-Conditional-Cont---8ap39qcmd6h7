93c8fef8da2b6ef824369e7756d988caffe8d08e
mkgupta2000 <105657874+mkgupta2000@users.noreply.github.com>
2022-11-25 21:00:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+
+  const [data,setData] = useState(true)
+
   
   return (
     <div id="main">
+
+    <h1 id={"marco-polo"} >{data ? "Marco":"Polo"}</h1>
+    <button id='marco-polo-toggler' onClick={()=>setData(!data)}>{!data ? "Marco":"Polo"}</button>
 
     </div>
   )
